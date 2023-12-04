80e3ecfa154355ef2c04d29d5aa19d4de19084a2
MohammedGhaly <medooghaly7.com>
2023-12-04 18:05:28 +0000

    tasks [8:10] solved

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+  return Array.from(set)
+    .filter((el) => el.startsWith(startString))
+    .map((el) => el.slice(startString.length))
+    .join('-');
+}
