cd5444cb0c1423431bbe5cb46ccdb6ed9a572fda
MohammedGhaly <medooghaly7.com>
2023-12-04 18:09:52 +0000

    task 8 fixed

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') return '';
   return Array.from(set)
-    .filter((el) => el.startsWith(startString))
+    .filter((el) => typeof el === 'string' && el.startsWith(startString))
     .map((el) => el.slice(startString.length))
     .join('-');
 }
